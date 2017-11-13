79b2e16f78edd245198dd002fcea14fbf56a2b42
amdsrinivas <amd.srinivas@gmail.com>
2017-11-13 22:16:45 +0530

    Improved the least cost function.

--- ComputerNetworks/CAFS/cafs.py
+++ ComputerNetworks/CAFS/cafs.py
@@ -12,24 +12,48 @@ visited = []
 done = False
 
 
-def find_distance(x1, x2, y1, y2):
-    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+def distance_btw_routers(router1, router2):
+    return math.sqrt((routers[router1].x - routers[router2].x)**2 + (routers[router1].y - routers[router2].y)**2)
+
+
+def energy_cost(n, d):
+    costs = {}
+    for item in n:
+        costs[item] = router_range * d[item]
+    return costs
+
+
+def distance_cost(s, n):
+    costs = {}
+    for item in n:
+        costs[item] = math.sqrt((routers[s].x - routers[item].x)**2 + (routers[s].y - routers[item].y)**2)
+    return costs
+
+
+def p_costs(s, n):
+    costs = {}
+    for item in n:
+        costs[item] = 1 - distance_btw_routers(item, receiver_index)/distance_btw_routers(s, receiver_index)
+    return costs
 
 
 def least_cost(s, n):
-    distance = {}
     while 1:
+        final_costs = {}
+        distance = distance_cost(s, n)
+        energy = energy_cost(n, distance)
+        p_cost = p_costs(s, n)
         for item in n:
-            distance[item] = math.sqrt((routers[s].x - routers[item].x)**2 + (routers[s].y - routers[item].y)**2)
-        if distance == {}:
+            final_costs[item] = energy[item]/p_cost[item]
+        if final_costs == {}:
             return -1
-        ind = min(distance, key=distance.get)
+        ind = min(final_costs, key=final_costs.get)
         print(ind)
         print(visited)
         print(n)
         if ind in visited:
             del n[n.index(ind)]
-            del distance[ind]
+            #del distance[ind]
         else:
             #visited.append(ind)
             return ind
@@ -64,7 +88,7 @@ class Router(threading.Thread):
 
     def find_neighbours(self):
         for item in routers:
-            if self.index != item.index and find_distance(self.x, item.x, self.y, item.y) <= 20:
+            if self.index != item.index and distance_btw_routers(self.index, item.index) <= 20:
                 self.neighbours.append(item.index)
 
     def print_status(self):
@@ -89,13 +113,14 @@ class Router(threading.Thread):
                     current_index = least_cost(self.index, routers[self.index].neighbours)
                     if current_index == -1:
                         print("No connectivity.")
+                        done =True
                     #del routers[self.index].neighbours[routers[self.index].neighbours.index(current_index)]
                     else:
                         self.Sender = 0
                         routers[current_index].Sender = 1
                         routers[current_index].buffer = self.buffer
                         print(" Data received : {0} from Router {1} to Router {2}"
-                          .format(routers[current_index].buffer, self.index, current_index))
+                              .format(routers[current_index].buffer, self.index, current_index))
                         routers[current_index].start()
 
 
